530e5afde6938f310cfea7b58f001fa414097879
Evgenygroup <mailfrom61@gmail.com>
2023-03-15 21:09:45 +0100

    adds lesson 2

---
+++ basic_programming/lesson_2/code/main_2/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World ");
+        System.out.println("Hello World ");
+        System.out.println("Hello world");
+       // type name
+        /*
+        dsgfhjgjkg
+        sdfghjk
+        ycdbn
+         */
+       // a=3;
+        int a;
+        String b;
+
+        // name = value
+        a=10;
+
+        int c = 5;
+
+        int e;
+        e=2;
+        System.out.println(a);
+        a=1;
+        System.out.println(a);
+
+        int d = a+1;
+         d = a+e;
+        System.out.println(d);
+
+        a=a+1;
+        System.out.println("Hello "+" World");
+
+
+
+
+
+    }
+
+
+
+}
---
+++ basic_programming/lesson_2/code/main_2/src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+ //   переменная1;
+
+ //   переменная2;
+
+ //   метод1
+
+ //   метод2
+    void print(){
+        System.out.println("Hello World");
+    }
+}
