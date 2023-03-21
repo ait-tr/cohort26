659666c84716ef76b6d26f0475749edd029194a7
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-03-21 21:01:00 +0100

    lesson update

--- basic_programming/lesson_6/code/ticket/src/Main.java
+++ basic_programming/lesson_6/code/ticket/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Расчет средней стоимости поездки");
@@ -10,7 +13,23 @@ public class Main {
         //    посчитать и округлить общее число поездок в месяц
         //    и посчитать среднюю стоимость одной поездки по проездному.
 
-            float f = 9876543210F;
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите стоимость проездного на месяц: ");
+        double priceMonatsCart = scanner.nextDouble();
+        System.out.println("Введите количество рабочих дней: ");
+        int workingDays = scanner.nextInt();
+
+        int trip = workingDays * 2;
+        int holidays = 31 - workingDays; // это для месяца март
+
+        int holidayTrips = (int) Math.round(holidays * 1.5);
+
+        trip = trip + holidayTrips; //  оператор присвоения , раньше присали  :=
+
+        System.out.println("Среднее количество поездок за прошедший месяц: " + trip);
+
+        double averageTripPrise = priceMonatsCart/trip;
+        System.out.println("Средняя стоимсоть поездки: " + averageTripPrise + " евро.");
 
     }
 }
