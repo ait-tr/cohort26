83a6c0fd7516f234f6ba0209e9be631c9dcf0b75
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-03-22 17:30:15 +0100

    lesson update

---
+++ basic_programming/lesson_7/code/srting_practicum/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Класс String - практикум ");
+
+        String st = "Привет!";
+        int l = st.length();
+
+        char ch = st.;
+
+
+    }
+}
