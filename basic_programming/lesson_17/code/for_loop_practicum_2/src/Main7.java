17563f8785a36dda3630f2bc4141b717a2bc537c
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-04-05 21:26:08 +0200

    consultation update

--- basic_programming/lesson_17/code/for_loop_practicum_2/src/Main7.java
+++ basic_programming/lesson_17/code/for_loop_practicum_2/src/Main7.java
@@ -1,8 +1,26 @@
+import java.util.Arrays;
+
 public class Main7 {
     public static void main(String[] args) {
-        System.out.println("Ruletka");
+        System.out.println("Chess desk");
+        // Заполнение полей шахматной доски. Двумерный массив.
+        // 1 - черное поле, 0 - белое поле
 
+        int[][] chessDesk = new int[8][8];
+        // chessDesk[i][j] - это элементы двумерного массива
 
+        for (int i = 0; i < chessDesk.length; i++) {
+            for (int j = 0; j < chessDesk[0].length; j++) {
+                chessDesk[i][j] = (j + i) % 2;
+                System.out.print(chessDesk[i][j] + " | ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println(chessDesk[0][0]);
+        System.out.println(chessDesk[7][7]);
+
+        System.out.println(Arrays.deepToString(chessDesk));
 
     }
 }
