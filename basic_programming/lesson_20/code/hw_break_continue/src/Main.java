26a27f6ced58656a189d9d6470192d942fc7185d
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-04-12 18:59:19 +0200

    lesson update

--- basic_programming/lesson_20/code/hw_break_continue/src/Main.java
+++ basic_programming/lesson_20/code/hw_break_continue/src/Main.java
@@ -4,6 +4,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        Хозяйка налепила для гостей 30 пельменей. В один из пельменей она положила монету,
+//        которая увеличивает вес пельменя на 15 грамм.
+//        Напишите программу, которая ищет счастливый пельмень.
+
+        int[] dumplings = new int[30];
+
+        for (int i = 0; i < dumplings.length; i++) {
+            dumplings[i] = (int)(Math.random() * (23 - 18 + 1) + 18); // задумываем веса пельменей в диапазоне 18-23 грамма
+        }
+        System.out.println(Arrays.toString(dumplings)); // напечатали для контроля веса пельменей
+        int luckyDumplingIndex = (int)(Math.random() * (29 - 0 + 1) + 0);// задумываем индекс счастливого пельменя
+        dumplings[luckyDumplingIndex] = dumplings[luckyDumplingIndex] + 15; // вкладываем в счастливый пельмень монету 15 грамм
+        System.out.println(Arrays.toString(dumplings)); // напечатали для контроля веса пельменей после закладки монеты
+        int index = 0; // переменная для индекса счастливого пельменя
+        // перебираем все пельмени, ищем "тяжелый", обрываем цикл как только он найден
+        for (int i = 0; i < dumplings.length; i++) {
+            if (dumplings[i] == dumplings[luckyDumplingIndex]) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Index of Lucky Dumpling is: " + index);
+
+    }
+}
+
+/*
+
+
 //        Задача 1
 //        Вводится натуральное число n с клавиатуры. Подсчитать сумму всех
 //        натуральных чисел меньших n, за исключением чисел, кратных числу 3.
@@ -18,33 +48,13 @@ public class Main {
                 continue;
             }
             sum = sum + i;
-            System.out.println("Index: " + i + " Sum: " + sum);
+            System.out.println("Index: " + i + " Sum: " + sum); // промежуточная печать
         }
         System.out.println("Sum is of numbers less then " + n + " w/out multiply of 3 is: " + sum);
         System.out.println();
 
-//        Хозяйка налепила для гостей 30 пельменей. В один из пельменей она положила монету,
-//        которая увеличивает вес пельменя на 15 грамм.
-//        Напишите программу, которая ищет счастливый пельмень.
 
-        int[] dumplings = new int[30];
 
-        for (int i = 0; i < dumplings.length; i++) {
-            dumplings[i] = (int)(Math.random() * (33 - 28 + 1) + 28); // задумываем веса пельменей в диапазоне 28-33 грамма
-        }
-        System.out.println(Arrays.toString(dumplings)); // напечатали для контроля веса пельменей
-        int luckyDumplingIndex = (int)(Math.random() * (30 - 1 + 1) + 1);// задумываем индекс счастливого пельменя
-        dumplings[luckyDumplingIndex] = dumplings[luckyDumplingIndex] + 15; // вкладываем в счастливый пельмень монету 15 грамм
-        System.out.println(Arrays.toString(dumplings)); // напечатали для контроля веса пельменей после закладки монеты
-        int index = 0; // переменная для индекса счастливого пельменя
-        // перебираем все пельмени, ищем "тяжелый", обрываем цикл как только он найден
-        for (int i = 0; i < dumplings.length; i++) {
-            if (dumplings[i] > 35) {
-                index = i;
-                break;
-            }
-        }
-        System.out.println();
-        System.out.println("Index of Lucky Dumpling is: " + index);
-    }
-}
+
+
+ */
