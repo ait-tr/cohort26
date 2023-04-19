33b75565de86fbfc89443f3310b3a21a47be6a4f
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-04-19 22:02:31 +0300

    lesson update

--- basic_programming/lesson_25/code/phone_book/src/Main.java
+++ basic_programming/lesson_25/code/phone_book/src/Main.java
@@ -1,12 +1,107 @@
-import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
        System.out.println("Phone book");
+//        Создайте телефонную книгу нашей группы со следющим функционалом:
+//        - добавить контакт (имя и телефон)
+//        - найти контакт
+//        - вывести(напечатать) все контакты
+//        - удалить контакт
 
-       Scanner scanner = new Scanner(System.in);
+    // Шаг 1 - сделать костяк по выбору из меню и собственно меню
+    // Шаг 2 - сделаем методы для выполнения задач из меню
+
+        Scanner scanner = new Scanner(System.in);
+        int userChoise = 0;
+        HashMap<String, String> phoneBook = new HashMap<>();
+
+        do {
+            printMenu();
+            userChoise = scanner.nextInt();
+
+            switch (userChoise) {
+                case 1: {
+                    // add
+                    addContact(phoneBook);
+                    break;
+                }
+                case 2: {
+                    // show
+                    printPhoneBook(phoneBook);
+                    break;
+                }
+                case 3: {
+                    // search
+                    System.out.println(searchPhoneBook(phoneBook));
+
+                    break;
+                }
+
+                case 4: {
+                    // 4-й пункт меню
+                    break;
+                }
+
+                case 5: {
+                    // Exit
+                    return;
+                }
+
+            }
+
+        } while(true);
+
+    }
+    //_________Methods_____________________
+// печатаем меню
+    public static void printMenu() {
+        System.out.println("1 - add, 2 - show, 3 - search, 4 - delete, 5 - exit");
+        System.out.println("Input your choice: ");
+    }
+
+    // метод добавляет контакт
+    public static HashMap<String, String> addContact(HashMap<String, String> contacts)  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input name: ");
+        String name = scanner.next();
+        System.out.println("Input phone number: ");
+        String phone = scanner.next();
+        contacts.put(name, phone);
+        return contacts;
+    }
+
+    public static void printPhoneBook (HashMap<String, String> contacts){
+        // цикл for each переберет весь HashMap по ключу и напечатает
+        for (String name: contacts.keySet()) {
+            System.out.println("Name: "+ name + " | Phone number: " + contacts.get(name));
+        }
+    }
+
+    // метод для поиска телефона по имени
+    public static String searchPhoneBook(HashMap<String, String> contacts) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input name: ");
+        String name = scanner.next();
+       // for (String n : contacts.keySet()) { // перебор всех значений
+            if (contacts.containsKey(name)) { // если совпало
+       //         System.out.println(n + " | " + contacts.get(n));
+                return contacts.get(name);
+            }
+
+        return null;
+    }
+
+    //_________end of Methods_____________________
+}
+
+
+
+
+
+/*
+ Scanner scanner = new Scanner(System.in);
        int userChoice;
 
        HashMap<String, String> phoneBook = new HashMap<>();
@@ -52,11 +147,7 @@ public class Main {
            }
        }while (true); // конец цикла
 
-    }
-
-    //_________Methods_____________________
-
-    // печатаем меню
+     // печатаем меню
     public static void menu() {
         System.out.println("Menu:");
         System.out.println("1 - add, 2 - show, 3 - find, 4 - delete contact, 5 - exit");
@@ -92,5 +183,5 @@ public class Main {
 
     }
 
-    //_________end of Methods_____________________
-}
+
+ */
