1cf17211e4065f1138d5705ed22dd58951406acd
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-25 17:40:03 +0200

    add Homework28 template

---
+++ basic_programming/lesson_29/code/files/src/Homework28.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Homework28 {
+
+  // Написать метод
+  //
+  // public static String decToBin(int number)
+  //
+  // который получит число и вернёт строку с его двоичной записью.
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите целое число: ");
+    int number = Integer.parseInt(bufferedReader.readLine());
+
+    String binNumberStr = decToBin(number);
+    System.out.println("Число " + number + " в системе счисления с основанием 2: " + binNumberStr);
+  }
+
+  public static String decToBin(int number) {
+    String result = "";
+
+    return result;
+  }
+}
