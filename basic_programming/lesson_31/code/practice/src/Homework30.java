1b7cbb4771b880f446abd33b68329ca7cbaa0103
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-27 18:01:55 +0200

    Add getSeason and getSeasonFromMap

--- basic_programming/lesson_31/code/practice/src/Homework30.java
+++ basic_programming/lesson_31/code/practice/src/Homework30.java
@@ -32,6 +32,33 @@ public class Homework30 {
     FALL, // AUTUMN
   }
 
+  private static Season getSeason(Month month) {
+    return switch (month) {
+      case DECEMBER, JANUARY, FEBRUARY -> Season.WINTER;
+      case MARCH, APRIL, MAY -> Season.SPRING;
+      case JUNE, JULY, AUGUST -> Season.SUMMER;
+      case SEPTEMBER, OCTOBER, NOVEMBER -> Season.FALL;
+    };
+  }
+
+  private static Season getSeasonFromMap(Month month) {
+    Map<Month, Season> seasonPerMonth = new HashMap<>();
+    seasonPerMonth.put(Month.JANUARY, Season.WINTER);
+    seasonPerMonth.put(Month.FEBRUARY, Season.WINTER);
+    seasonPerMonth.put(Month.MARCH, Season.SPRING);
+    seasonPerMonth.put(Month.APRIL, Season.SPRING);
+    seasonPerMonth.put(Month.MAY, Season.SPRING);
+    seasonPerMonth.put(Month.JUNE, Season.SUMMER);
+    seasonPerMonth.put(Month.JULY, Season.SUMMER);
+    seasonPerMonth.put(Month.AUGUST, Season.SUMMER);
+    seasonPerMonth.put(Month.SEPTEMBER, Season.FALL);
+    seasonPerMonth.put(Month.OCTOBER, Season.FALL);
+    seasonPerMonth.put(Month.NOVEMBER, Season.FALL);
+    seasonPerMonth.put(Month.DECEMBER, Season.WINTER);
+
+    return seasonPerMonth.get(month);
+  }
+
   public static void main(String[] args) {
     Map<Month, Integer> daysPerMonth = new HashMap<>();
     daysPerMonth.put(Month.JANUARY, 31);
@@ -51,6 +78,8 @@ public class Homework30 {
     System.out.print("Enter the month: ");
     String month = scanner.next(); // месяц "как ввели (как строка)"
     Month monthKey = Month.valueOf(month.toUpperCase()); // месяц "как enum"
-    System.out.println(month + " has " + daysPerMonth.get(monthKey) + " days");
+    System.out.println(month + " has " + daysPerMonth.get(monthKey) + " days.");
+    System.out.println(month + " is a part of the " + getSeason(monthKey) + " season.");
+    System.out.println(month + " is a part of the " + getSeasonFromMap(monthKey) + " season.");
   }
 }
