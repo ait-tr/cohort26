24015904b9495c78da22baec62486ead91c1efd3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-27 17:34:50 +0200

    Add Homework30 template

--- basic_programming/lesson_31/code/practice/src/Homework30.java
+++ basic_programming/lesson_31/code/practice/src/Homework30.java
@@ -1,6 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Homework30 {
 
+  // Расширьте программу `Task2MonthsEnums`.
+  //
+  // Добавьте ещё один `enum`, который будет описывать времена года (зима, лето, весна, осень).
+  //
+  // Для введённого пользователем месяца напишите, к какому именно времени года относится
+  // выбранный месяц.
+  private enum Month {
+    JANUARY, // 0
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER,
+  }
+  // Для Java enum - это набор констант, идущих подряд. Все эти константы пронумерованы,
+  // начиная с нуля.
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // в программе созданный enum можно использовать, как новый класс - как новый тип переменной
+    // (как int, String и т.д.)
+    Map<Month, Integer> daysPerMonth = new HashMap<>();
+    daysPerMonth.put(Month.JANUARY, 31);
+    daysPerMonth.put(Month.FEBRUARY, 28); // год не високосный
+    daysPerMonth.put(Month.MARCH, 31);
+    daysPerMonth.put(Month.APRIL, 30);
+    daysPerMonth.put(Month.MAY, 31);
+    daysPerMonth.put(Month.JUNE, 30);
+    daysPerMonth.put(Month.JULY, 31);
+    daysPerMonth.put(Month.AUGUST, 31);
+    daysPerMonth.put(Month.SEPTEMBER, 30);
+    daysPerMonth.put(Month.OCTOBER, 31);
+    daysPerMonth.put(Month.NOVEMBER, 30);
+    daysPerMonth.put(Month.DECEMBER, 31);
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the month: ");
+    String month = scanner.next(); // месяц "как ввели"
+    // enum.valueOf("ЗНАЧЕНИЕ") либо вернёт нам соответствующее значение по его названию,
+    // либо выбросит исключение IllegalArgumentException
+    Month monthKey = Month.valueOf(month.toUpperCase()); // месяц "как ключ словаря"
+    // enum.ordinal() возвращает индекс значения в перечислении
+    System.out.println(month + " has " + daysPerMonth.get(monthKey) + " days");
   }
 }
