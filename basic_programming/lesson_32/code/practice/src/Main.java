164b53bfb172315a11262c2e5f77a300588d78eb
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-28 18:14:01 +0200

    feat: add commit message convention

--- basic_programming/lesson_32/code/practice/src/Main.java
+++ basic_programming/lesson_32/code/practice/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 
+  // сообщения о коммитах (commit messages):
+  // - должны быть на английском
+  // - должны начинаться с глагола в повелительном наклонении
+  // "добавь файл" - add file
+  // "добавь метод" - add method
+  // "исправь код" - fix the code
+  // - не должны быть общими:
+  // вместо "добавь метод" стоит писать "добавь decToBin()"
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
