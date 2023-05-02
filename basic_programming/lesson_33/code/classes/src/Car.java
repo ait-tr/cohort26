748b619d2b2cc1af09a9897e5438fcc417e59ab5
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-02 20:05:47 +0200

    feat: add Car

---
+++ basic_programming/lesson_33/code/classes/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+
+  // атрибуты пишутся отдельно, лучше в начале класса
+  // private тип название;
+  public String manufacturer; // производитель
+  public String color; // цвет
+  public int power; // мощность
+}
