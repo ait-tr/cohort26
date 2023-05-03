1c3fab6bcf9e2ba23146567f70db06b504334524
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-03 17:34:41 +0200

    feat: add homework33 template

---
+++ basic_programming/lesson_34/code/access/src/homework33/Main.java
@@ -0,0 +1,13 @@
+package homework33;
+
+public class Main {
+
+  // Используйте класс Soda в вашей программе:
+  // - создайте газировку
+  // - выведите её добавку
+  // - измените добавку
+  // - выведите новую добавку
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
