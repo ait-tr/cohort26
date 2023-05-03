5815a336e2706facd437b71674f9eff7bb297b51
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-03 18:34:23 +0200

    feat: add Pizza name check

--- basic_programming/lesson_34/code/access/src/Pizza.java
+++ basic_programming/lesson_34/code/access/src/Pizza.java
@@ -8,9 +8,16 @@ public class Pizza {
   private int price;
 
   public Pizza(String name, int price) {
+    if (name == null || name.isEmpty()) {
+      throw new IllegalArgumentException("Название не может быть пустым");
+    }
     this.name = name; // это не изменение, это задание первоначального значения - инициализация
     if (price < 0) { // условие-стражник (guardian condition)
       throw new IllegalArgumentException("Цена не может быть отрицательной");
+      // throw какаяТоОшибка - оператор "завершиться с ошибкой"
+      // RuntimeException - какая-то ошибка во время выполнения
+      // IllegalArgumentException - некорректный аргумент метода
+      // В аргументах конструктора исключения указывается строка - сообщение об ошибке
     }
     this.price = price;
   }
