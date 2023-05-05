8cf56ea7114a9b899ed04a318e6ed553be37fdd3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-05 20:11:30 +0200

    feat: add orders number

--- basic_programming/lesson_36/code/practice/src/GasStation.java
+++ basic_programming/lesson_36/code/practice/src/GasStation.java
@@ -21,33 +21,45 @@ public class GasStation {
     System.out.println("Добро пожаловать! Как вас зовут?");
     String name = scanner.nextLine();
 
-    System.out.println("Сколько литров топлива вам нужно?");
-    double quantity = 0.0;
-    // пока всё плохо, пропустить строку
-    while (!scanner.hasNextDouble()) {
+    System.out.print("Введите количество заказов: ");
+    while (!scanner.hasNextInt()) {
       String wrongLine = scanner.nextLine();
-      System.out.println("Неправильный формат дробного числа: " + wrongLine);
-      System.out.println("Введите количество топлива (в литрах):");
+      System.out.println("Неправильный формат целого числа: " + wrongLine);
+      System.out.print("Введите количество заказов: ");
     }
+    int orders = scanner.nextInt();
+
+    // for (командаПередЦиклом; условиеПовторения; командаПослеШага)
+    for (int counter = 0; counter < orders; ++counter) {
+      System.out.println("Сколько литров топлива вам нужно?");
+//    double quantity = 0.0; эта строчка была нужна, когда значение quantity присваивалось
+      // в цикле или условии
+      // пока всё плохо, пропустить строку
+      while (!scanner.hasNextDouble()) {
+        String wrongLine = scanner.nextLine();
+        System.out.println("Неправильный формат дробного числа: " + wrongLine);
+        System.out.println("Введите количество топлива (в литрах):");
+      }
 //    if (scanner.hasNextDouble()) { // если всё хорошо
-    quantity = scanner.nextDouble();
+      double quantity = scanner.nextDouble();
 //    }
-    scanner.nextLine();
+      scanner.nextLine();
 
-    System.out.println("Как будете оплачивать, картой или наличными?");
-    String paymentMethod = scanner.nextLine();
-    // флажок (flag) - булева переменная, которая хранит в себе какой-то признак
-    boolean byCard = paymentMethod.toLowerCase().contains("карт");
+      System.out.println("Как будете оплачивать, картой или наличными?");
+      String paymentMethod = scanner.nextLine();
+      // флажок (flag) - булева переменная, которая хранит в себе какой-то признак
+      boolean byCard = paymentMethod.toLowerCase().contains("карт");
 
-    System.out.println("=== Ваш заказ ===");
-    System.out.println("Заказчик: " + name);
-    System.out.println("Количество топлива: " + quantity + " л");
-    double total = quantity * 2;
-    System.out.println("Стоимость заказа: " + total + " EUR");
-    if (byCard) {
-      System.out.println("Заказ должен быть оплачен картой");
-    } else {
-      System.out.println("Заказ должен быть оплачен наличными");
+      System.out.println("=== Ваш заказ ===");
+      System.out.println("Заказчик: " + name);
+      System.out.println("Количество топлива: " + quantity + " л");
+      double total = quantity * 2;
+      System.out.println("Стоимость заказа: " + total + " EUR");
+      if (byCard) {
+        System.out.println("Заказ должен быть оплачен картой");
+      } else {
+        System.out.println("Заказ должен быть оплачен наличными");
+      }
     }
   }
 }
