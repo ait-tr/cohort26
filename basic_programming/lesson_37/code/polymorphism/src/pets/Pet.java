34848f0aaa2520d4279539ae97b097b44cde185f
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-08 18:47:22 +0200

    feat: add Pet and Cat

---
+++ basic_programming/lesson_37/code/polymorphism/src/pets/Pet.java
@@ -0,0 +1,18 @@
+package pets;
+
+public class Pet {
+
+  private String name;
+
+  public Pet(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
