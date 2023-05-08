30aa7c3caa3b9a233db55a9d96d4e9d536dcc6f7
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-08 17:56:58 +0200

    fix: add non-negative orders amount check

--- basic_programming/lesson_37/code/polymorphism/src/homework_36/GasStation.java
+++ basic_programming/lesson_37/code/polymorphism/src/homework_36/GasStation.java
@@ -26,25 +26,32 @@ public class GasStation {
     System.out.println("Добро пожаловать! Как вас зовут?");
     String name = scanner.nextLine();
 
-    int orders = readOrdersAmount(scanner);
+    int amount = readOrdersAmount(scanner);
+    Order[] orders = new Order[amount];
 
     // for (командаПередЦиклом; условиеПовторения; командаПослеШага)
-    for (int counter = 0; counter < orders; ++counter) {
+    for (int counter = 0; counter < amount; ++counter) {
       Order order = Order.readOrder(name, scanner);
       order.print();
     }
   }
 
   private static int readOrdersAmount(Scanner scanner) {
-    System.out.print("Введите количество заказов: ");
-    while (!scanner.hasNextInt()) {
-      String wrongLine = scanner.nextLine();
-      System.out.println("Неправильный формат целого числа: " + wrongLine);
+    int amount; // объявляем переменную заранее, чтобы она осталась после цикла
+    do { // читаем количество заказов
       System.out.print("Введите количество заказов: ");
-    }
-    int orders = scanner.nextInt();
-    scanner.nextLine(); // пропустить остаток строки, из которой мы прочитали число
-    // сканер не закрываем - мы продолжим им пользоваться, не мы создавали, не нам закрывать
-    return orders;
+      while (!scanner.hasNextInt()) { // пока написано не число (подсматриваем вперёд)
+        String wrongLine = scanner.nextLine(); // пропускаем ввод (там не число)
+        System.out.println("Неправильный формат целого числа: " + wrongLine);
+        System.out.print("Введите количество заказов: ");
+      }
+      amount = scanner.nextInt(); // пропустили все "не числа", теперь читаем число
+      scanner.nextLine(); // пропустить остаток строки, из которой мы прочитали число
+      // сканер не закрываем - мы продолжим им пользоваться, не мы создавали, не нам закрывать
+      if (amount < 0) { // если только что прочитанное количество меньше нуля
+        System.out.println("Количество заказов не может быть отрицательным: " + amount);
+      }
+    } while (amount < 0); // если количество отрицательное - читаем заново
+    return amount;
   }
 }
