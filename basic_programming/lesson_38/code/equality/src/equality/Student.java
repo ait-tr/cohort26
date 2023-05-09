4f28d3448a7873e9327876bd79f61b6afd284684
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-09 20:43:24 +0200

    feat: improve Student.equals

--- basic_programming/lesson_38/code/equality/src/equality/Student.java
+++ basic_programming/lesson_38/code/equality/src/equality/Student.java
@@ -3,6 +3,7 @@ package equality;
 public class Student {
 
   private String name;
+  private int age;
 
   public Student(String name) {
     this.name = name;
@@ -14,6 +15,10 @@ public class Student {
 
   public String getName() {
     return name;
+  }
+
+  public int getAge() {
+    return age;
   }
 
   @Override
@@ -30,10 +35,19 @@ public class Student {
     if (this == obj) { // this -- это ссылка на текущий объект
       return true; // сравниваем объект сам с собой
     }
-    if (!(obj instanceof Student student)) { // является экземпляром класса Student
+    if (!(obj instanceof Student objStudent)) { // является экземпляром класса Student
       // instance - пример, экземпляр, образец
       return false; // если объект в скобочках - не студент (не класс Student или его наследник)
     }
-    return name.equals(student.getName());
+    // выше произошло:
+    // Student objStudent = (Student) obj;
+    // чтобы получить доступ к методам класса
+
+    // здесь определение того, что мы считаем совпадающими (или одинаковыми) объектами
+    // это один и тот же студент, если совпадают имя и возраст
+    return name.equals(objStudent.getName()) && age == objStudent.getAge();
+
+    // В методе `equals` для класса чаще всего в конце просто вызываются методы
+    // `equals` для атрибутов этого класса и их результат объединяется через && (логическое И)
   }
 }
