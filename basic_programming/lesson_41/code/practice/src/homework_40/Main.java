723850ea1bc482425c511eed3020c91d0166572b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-12 18:38:08 +0200

    feat: refactor Main, add makeThem* methods

--- basic_programming/lesson_41/code/practice/src/homework_40/Main.java
+++ basic_programming/lesson_41/code/practice/src/homework_40/Main.java
@@ -11,13 +11,26 @@ public class Main {
     // есть объект (где-то в памяти)
     Superman clarkKent = new Superman();
 
-    CanFly flyableObject = clarkKent;
-    flyableObject.fly();
+    makeThemFly(clarkKent);
+    makeThemRun(clarkKent);
+    makeThemSwim(clarkKent);
+  }
 
-    CanRun runnableObject = clarkKent;
-    runnableObject.run();
+  // метод "заставь летать"
+  private static void makeThemFly(CanFly flyableObject) {
+    System.out.println("Main.makeThemFly(flyableObject=" + flyableObject + ")");
+    flyableObject.fly(); // команда "лети!"
+  }
 
-    CanSwim smimmableObject = clarkKent;
-    smimmableObject.swim();
+  // метод "заставь бежать"
+  private static void makeThemRun(CanRun runnableObject) {
+    System.out.println("Main.makeThemRun(runnableObject=" + runnableObject + ")");
+    runnableObject.run(); // команда "беги!"
+  }
+
+  // метод "заставь плыть"
+  private static void makeThemSwim(CanSwim swimmableObject) {
+    System.out.println("Main.makeThemSwim(swimmableObject=" + swimmableObject + ")");
+    swimmableObject.swim(); // команда "плыви!"
   }
 }
