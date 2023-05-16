9016d5fff4da1a164f9d8d5339be91c5bae590a5
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-16 18:02:53 +0200

    feat: add homework_42.Book.compareTo()

--- basic_programming/lesson_43/code/comparators/src/homework_42/Book.java
+++ basic_programming/lesson_43/code/comparators/src/homework_42/Book.java
@@ -1,12 +1,9 @@
 package homework_42;
 
-public class Book {
+public class Book implements Comparable<Book> {
 
   // Создайте класс `Book` (книга), в конструктор которого передавайте автора, название книги и
   // количество страниц.
-  //
-  // Реализуйте интерфейс для сравнения книг: они должны сортироваться по авторам,
-  // а если авторы совпадают - по названиям (и там, и там - по алфавиту, "в словарном порядке").
   private final String author;
   private final String title;
   private final int pages;
@@ -27,5 +24,33 @@ public class Book {
 
   public int getPages() {
     return pages;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{author='" + author + "', title='" + title + "', pages=" + pages + "}";
+  }
+
+  // Реализуйте интерфейс для сравнения книг: они должны сортироваться по авторам,
+  // а если авторы совпадают - по названиям (и там, и там - по алфавиту, "в словарном порядке").
+
+  /**
+   * Сравнивает книги по автору (в лексикографическом порядке), а при одинаковых авторах - по
+   * названию (без учёта регистра)
+   *
+   * @param o the object to be compared.
+   * @return отрицательное число, если книга расположена раньше в упорядоченном списке, 0 для
+   * одинаковых книг и положительное число, если книга расположена позже в упорядоченном списке
+   */
+  @Override
+  public int compareTo(Book o) {
+    String author1 = this.author.toLowerCase();
+    String author2 = o.author.toLowerCase();
+    if (!author1.equals(author2)) { // если авторы не одинаковы
+      return author1.compareTo(author2); // сравнение авторов в алфавитном порядке
+    }
+    // сюда попали, только если авторы одинаковы
+    // сравнение названий в алфавитном порядке
+    return title.toLowerCase().compareTo(o.title.toLowerCase());
   }
 }
--- basic_programming/lesson_43/code/comparators/src/homework_42/Main.java
+++ basic_programming/lesson_43/code/comparators/src/homework_42/Main.java
@@ -1,8 +1,18 @@
 package homework_42;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    List<Book> books = new ArrayList<>();
+    books.add(new Book("Пушкин", "Руслан и Людмила", 125));
+    books.add(new Book("Пушкин", "Дубровский", 126));
+    books.add(new Book("Лермонтов", "Мцыри", 127));
+    System.out.println(books);
+    Collections.sort(books);
+    System.out.println(books);
   }
 }
