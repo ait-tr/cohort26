4bacd7a0561411dc06957b3d8ad13f4af5a1d6c1
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-16 19:06:32 +0200

    feat: add Task2BookPrices solution

--- basic_programming/lesson_43/code/comparators/src/Task2BookPrices.java
+++ basic_programming/lesson_43/code/comparators/src/Task2BookPrices.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Task2BookPrices {
 
   public static void main(String[] args) {
     Map<Book, Integer> prices = new HashMap<>();
+    prices.put(new Book("Пушкин", "Руслан и Людмила", 125), 500);
+    prices.put(new Book("Пушкин", "Дубровский", 126), 550);
+    prices.put(new Book("Лермонтов", "Мцыри", 127), 725);
+    prices.put(new Book("Пушкин", "Дубровский", 126), 350); // перезаписал цену
+
+    System.out.println(prices);
+    List<Book> keys = new ArrayList<>(prices.keySet()); // скопировал ключи в отдельный список
+    keys.sort(new BookTitleComparator()); // отсортировал этот отдельный список ключей
+    for (Book book : keys) { // перебираю отдельный отсортированный список ключей
+      System.out.println(book + " --> " + prices.get(book)); // для каждого ключа получаю значение
+    }
   }
 }
