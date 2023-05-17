6641545955f1e1738eaa3b6e8b03ad552fc5ff9b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-17 19:01:43 +0200

    feat: add pizza.Order template

---
+++ basic_programming/lesson_44/code/tests/src/pizza/Order.java
@@ -0,0 +1,25 @@
+package pizza;
+
+import java.util.Scanner;
+
+public class Order {
+
+  // - номер заказа
+
+  // - для каждой пиццы:
+  //   - выбор пиццы
+  //   - выбор размера
+  //   - нужен ли сырный борт
+  public void addPizza(Scanner scanner) {
+    // TODO
+  }
+
+  public boolean isEmpty() {
+    return false; // TODO
+  }
+
+  // итоговая стоимость
+  public double getTotal() {
+    return 0.0; // TODO
+  }
+}
