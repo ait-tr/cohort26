875edbcbbeb00eb128eb308c7d35adde263c76ff
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-17 19:40:16 +0200

    feat: finish pizza.Order and add pizza.Pizza template

--- basic_programming/lesson_44/code/tests/src/pizza/Order.java
+++ basic_programming/lesson_44/code/tests/src/pizza/Order.java
@@ -1,25 +1,45 @@
 package pizza;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Order {
 
   // - номер заказа
+  private static int ordersAmount = 0; // счётчик созданных заказов
+  private final int number;
+  private final List<Pizza> pizzas = new ArrayList<>();
 
-  // - для каждой пиццы:
-  //   - выбор пиццы
-  //   - выбор размера
-  //   - нужен ли сырный борт
+  public Order() {
+    number = ++ordersAmount;
+  }
+
   public void addPizza(Scanner scanner) {
-    // TODO
+    pizzas.add(Pizza.readPizza(scanner));
   }
 
   public boolean isEmpty() {
-    return false; // TODO
+    return pizzas.isEmpty();
   }
 
   // итоговая стоимость
   public double getTotal() {
-    return 0.0; // TODO
+    double result = 0.0;
+    for (Pizza pizza : pizzas) {
+      result += pizza.getPrice();
+    }
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    String result =  "=== Заказ № " + number + "\n";
+//    pizzas.sort(new PizzaComparator()); // TODO
+    for (int i = 0; i < pizzas.size(); ++i) {
+//      result += (i + 1) + ". " + pizzas.get(i) + "\n";
+      result += String.format("%d. %s%n", i + 1, pizzas.get(i));
+    }
+    return result;
   }
 }
---
+++ basic_programming/lesson_44/code/tests/src/pizza/Pizza.java
@@ -0,0 +1,23 @@
+package pizza;
+
+import java.util.Scanner;
+
+public class Pizza {
+
+  // - для каждой пиццы:
+  //   - выбор пиццы
+  //   - выбор размера
+  //   - нужен ли сырный борт
+  public static Pizza readPizza(Scanner scanner) {
+    return new Pizza(); // TODO
+  }
+
+  public double getPrice() {
+    return 0.0; // TODO
+  }
+
+  @Override
+  public String toString() {
+    return ""; // TODO
+  }
+}
