de05be3fa162d3a0cd6c9c8d791c49058330a4af
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-17 17:52:34 +0200

    feat: add homework_43.BookTitleAuthorComparator usage

--- basic_programming/lesson_44/code/tests/src/homework_43/Main.java
+++ basic_programming/lesson_44/code/tests/src/homework_43/Main.java
@@ -1,5 +1,8 @@
 package homework_43;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   // Используйте класс `Book` (книга) из предыдущего домашнего задания.
@@ -7,6 +10,13 @@ public class Main {
   // Создайте компаратор, который позволит сортировать книги по названию,
   // при одинаковых названиях - по автору (и там, и там - по алфавиту)
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    List<Book> books = new ArrayList<>();
+    books.add(new Book("Пушкин", "Собрание сочинений", 527));
+    books.add(new Book("Пушкин", "Руслан и Людмила", 350));
+    books.add(new Book("Лермонтов", "Собрание сочинений", 435));
+    System.out.println(books);
+
+    books.sort(new BookTitleAuthorComparator());
+    System.out.println(books);
   }
 }
