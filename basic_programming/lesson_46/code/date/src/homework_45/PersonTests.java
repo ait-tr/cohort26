c509ef344b6baa20b9eaf01c90c7d91103b08b16
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-23 18:18:32 +0200

    feat: add Person.compareTo tests

--- basic_programming/lesson_46/code/date/src/homework_45/PersonTests.java
+++ basic_programming/lesson_46/code/date/src/homework_45/PersonTests.java
@@ -1,6 +1,7 @@
 package homework_45;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,5 +24,37 @@ public class PersonTests {
     // assert - предположение, что результат верен
     assertEquals(name, person.getName());
     assertEquals(birthdate, person.getBirthdate());
+  }
+
+  @Test
+  public void testNotEqual() throws ParseException {
+    // arrange
+    Person person1 = new Person("Test", "25.04.2023");
+    Person person2 = new Person("Test", "24.03.2022");
+
+    // act
+    int result12 = person1.compareTo(person2);
+    int result21 = person2.compareTo(person1);
+
+    // assert
+    assertTrue(result12 < 0); // person1 < person2
+    assertTrue(result21 > 0); // person2 > person1
+  }
+
+  @Test
+  public void testEqual() throws ParseException {
+    // arrange
+    Person person1 = new Person("Test", "25.04.2023");
+    Person person2 = new Person("Test", "25.04.2023");
+
+    // act
+    int result11 = person1.compareTo(person1);
+    int result12 = person1.compareTo(person2);
+    int result21 = person2.compareTo(person1);
+
+    // assert
+    assertEquals(0, result11); // person1 == person1
+    assertEquals(0, result12); // person1 == person2
+    assertEquals(0, result21); // person2 == person1
   }
 }
