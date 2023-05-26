2f9b7f66adff1af46c0950cfbb890d39a02f3694
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-26 19:09:39 +0200

    feat: add Order

---
+++ basic_programming/lesson_49/code/practice/src/Order.java
@@ -0,0 +1,50 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Order {
+
+  // Заказ:
+  // - ID // число
+  // - описание букета // строка (без переносов)
+  // - дата доставки // ДД.ММ.ГГГГ ЧЧ:ММ
+  private static final String SEP = ";";
+  private static final SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+
+  private static int counter = 0; // счётчик ОБЩИЙ для всех заказов
+
+  private final int id;
+  private String content;
+  private Date dueDate;
+
+  public Order(String content, String date) throws ParseException {
+    this.id = ++counter; // сначала увеличиваем счётчик, потом его значение становится ID
+    this.content = content;
+    this.dueDate = formatter.parse(date);
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public void setDueDate(String date) throws ParseException {
+    this.dueDate = formatter.parse(date);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public Date getDueDate() {
+    return dueDate;
+  }
+
+  @Override
+  public String toString() { // использую для записи в файл
+    return id + SEP + content + SEP + formatter.format(dueDate);
+  }
+}
