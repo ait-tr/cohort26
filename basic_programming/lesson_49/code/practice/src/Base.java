b7ca9bb51627ecd3dbda99c6f8180dc143e00286
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-26 20:07:27 +0200

    feat: refactor Base.add()

--- basic_programming/lesson_49/code/practice/src/Base.java
+++ basic_programming/lesson_49/code/practice/src/Base.java
@@ -14,22 +14,35 @@ public class Base {
   // - добавить заказ
   // - изменить заказ
   // - удалить заказ
+
+  // добавить заказ: работает с клавиатурой
   public void add() {
     Scanner scanner = new Scanner(System.in);
-
-    Order order = null;
+    boolean added = false; // флаг "заказ добавлен"
     System.out.print("Введите описание букета: ");
     String content = scanner.nextLine();
     do {
       try {
         System.out.print("Введите дату (в формате 'dd.MM.yyyy HH:mm': ");
         String date = scanner.nextLine();
-        order = new Order(content, date);
+        add(content, date); // работает с прочитанными данными
+        // если не случилось ошибки, то
+        added = true;
       } catch (ParseException e) {
         System.out.println("Некорректный формат даты: " + e.getMessage());
       }
-    } while (order == null);
+    } while (!added); // повторять, пока заказ не добавлен
+  }
 
-    orders.put(order.getId(), order);
+  // добавить заказ: работает с готовыми данными
+  public void add(String content, String date) throws ParseException {
+    Order order = new Order(content, date);
+    int orderId = order.getId();
+    // такого не должно быть, именно поэтому мы пишем что-то вроде теста
+    if (orders.containsKey(orderId)) {
+      // если произошло то, что никогда не должно происходить, выбрасываем ошибку
+      throw new RuntimeException("Заказ с id " + orderId + " уже существует");
+    }
+    orders.put(orderId, order);
   }
 }
