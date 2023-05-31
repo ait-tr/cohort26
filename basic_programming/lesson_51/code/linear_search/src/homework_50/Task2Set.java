2cfa1d2b8ea446096a679ddf750504a9dd649f76
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-31 17:45:09 +0200

    feat: add Task2Set solution (type 1)

--- basic_programming/lesson_51/code/linear_search/src/homework_50/Task2Set.java
+++ basic_programming/lesson_51/code/linear_search/src/homework_50/Task2Set.java
@@ -1,5 +1,9 @@
 package homework_50;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Task2Set {
 
   // Прочитать с клавиатуры сначала количество целых чисел, а затем сами числа.
@@ -7,6 +11,24 @@ public class Task2Set {
   // Удалить из множества все нечётные числа.
   // Вывести получившееся множество.
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    Set<Integer> numbers = new HashSet<>();
+    for (int i = 0; i < n; ++i) {
+//      int x = scanner.nextInt();
+//      numbers.add(x);
+      numbers.add(scanner.nextInt());
+    }
 
+    // удаление: 2 способа
+    // 1-й способ: перебор копии
+    Set<Integer> numbersCopy = new HashSet<>(numbers); // создаём копию numbers
+    for (int x : numbersCopy) { // перебираем копию
+      if (x % 2 != 0) {
+        numbers.remove(x); // меняем оригинал
+      }
+    }
+
+    System.out.println(numbers);
   }
 }
