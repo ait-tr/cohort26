e24c8ea7cf59e81ac4d2bbd04665b74793e2ddcf
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-31 21:00:36 +0200

    feat: add Task3Phonebook.indexOfPhone() and .lastIndexOfName()

--- basic_programming/lesson_51/code/linear_search/src/Task3Phonebook.java
+++ basic_programming/lesson_51/code/linear_search/src/Task3Phonebook.java
@@ -21,14 +21,20 @@ public class Task3Phonebook {
   // такой поиск называется линейным: прямой перебор всех объектов в поисках нужного
   // работаем с данными нашего класса, n для оценки эффективности - размер списка people
   // O(n) по времени, O(1) по дополнительной памяти
+//  public boolean hasName(String name) {
+//    for (Person person : people) { // перебираем всех людей // n раз = O(1 * n) = O(n)
+//      if (person.getName().equals(name)) { // если имя человека из списка совпадает с нужным // O(1)
+//        return true; // мы его нашли, можно заканчивать
+//      }
+//    }
+//    // если перебрали всех и не закончили, то никого не нашли
+//    return false;
+//  }
   public boolean hasName(String name) {
-    for (Person person : people) { // перебираем всех людей // n раз = O(1 * n) = O(n)
-      if (person.getName().equals(name)) { // если имя человека из списка совпадает с нужным // O(1)
-        return true; // мы его нашли, можно заканчивать
-      }
-    }
-    // если перебрали всех и не закончили, то никого не нашли
-    return false;
+    return indexOfName(name) != -1;
+    // Если мы нашли какой-то индекс, то он НЕ -1, и условие (индекс != -1) превратится в true;
+    // если мы ничего не нашли, то indexOf вернул нам -1, и условие (индекс != -1) будет false;
+    // мы избавились от дублирования кода: по-настоящему ищем мы теперь только в одном методе.
   }
 
   /**
@@ -44,6 +50,29 @@ public class Task3Phonebook {
 //      if (personName.equals(name)) {
 //        return i;
 //      }
+      if (people.get(i).getName().equals(name)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public int indexOfPhone(String phone) { // O(n) по времени, O(1) по дополнительной памяти
+    for (int i = 0; i < people.size(); ++i) {
+      if (people.get(i).getPhone().equals(phone)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public boolean hasPhone(String phone) { // O(n) по времени, O(1) по дополнительной памяти
+    return indexOfPhone(phone) != -1; // O(n) по времени, O(1) по дополнительной памяти
+  }
+
+  public int lastIndexOfName(String name) {
+    // для поиска последнего индекса перебираем индексы с конца (people.size() - 1) к началу (0)
+    for (int i = people.size() - 1; i >= 0; --i) {
       if (people.get(i).getName().equals(name)) {
         return i;
       }
