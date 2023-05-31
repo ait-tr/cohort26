f8efa8f0b1eb31814a4a22f11a4c2d73c966e313
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-31 18:15:23 +0200

    style: add Task2Set complexity comments

--- basic_programming/lesson_51/code/linear_search/src/homework_50/Task2Set.java
+++ basic_programming/lesson_51/code/linear_search/src/homework_50/Task2Set.java
@@ -14,29 +14,31 @@ public class Task2Set {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     Set<Integer> numbers = new HashSet<>();
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) { // n раз, каждый раз по O(1), итого - O(1 * n) = O(n)
 //      int x = scanner.nextInt();
 //      numbers.add(x);
-      numbers.add(scanner.nextInt());
+      numbers.add(scanner.nextInt()); // O(1) по времени
     }
 
     // удаление: 2 способа
     // 1-й способ: перебор копии
-//    Set<Integer> numbersCopy = new HashSet<>(numbers); // создаём копию numbers
-//    for (int x : numbersCopy) { // перебираем копию
-//      if (x % 2 != 0) {
-//        numbers.remove(x); // меняем оригинал
-//      }
-//    }
-    // 2-й способ: временное хранилище для всего, что надо удалить
-    Set<Integer> oddNumbersToRemove = new HashSet<>();
-    for (int x : numbers) { // перебираем числа
-      if (x % 2 != 0) { // если число нечётное
-        oddNumbersToRemove.add(x); // запоминаем его "для удаления"
+    Set<Integer> numbersCopy = new HashSet<>(numbers); // создаём копию numbers
+    for (int x : numbersCopy) { // перебираем копию - n элементов, для каждого O(1), итого O(n)
+      if (x % 2 != 0) { // O(1)
+        numbers.remove(x); // меняем оригинал // O(1)
       }
     }
-    numbers.removeAll(oddNumbersToRemove);
+    // O(n) + O(n) = O(2 * n) = O(n)
+    // 2-й способ: временное хранилище для всего, что надо удалить
+//    Set<Integer> oddNumbersToRemove = new HashSet<>();
+//    for (int x : numbers) { // перебираем числа
+//      if (x % 2 != 0) { // если число нечётное
+//        oddNumbersToRemove.add(x); // запоминаем его "для удаления"
+//      }
+//    }
+//    numbers.removeAll(oddNumbersToRemove);
 
-    System.out.println(numbers);
+    System.out.println(numbers); // O(n / 2) = O(1/2 * n) = O(0.5 * n) = O(n)
+    // O(n) + O(n) = O(n)
   }
 }
