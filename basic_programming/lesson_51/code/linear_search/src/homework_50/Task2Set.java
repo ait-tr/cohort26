9979c2c89b13c2931b6b64d47c635f4c660a432b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-05-31 18:54:02 +0200

    style: add Task2Set space complexity comments

--- basic_programming/lesson_51/code/linear_search/src/homework_50/Task2Set.java
+++ basic_programming/lesson_51/code/linear_search/src/homework_50/Task2Set.java
@@ -10,10 +10,10 @@ public class Task2Set {
   // Сохранить эти числа в множестве.
   // Удалить из множества все нечётные числа.
   // Вывести получившееся множество.
-  public static void main(String[] args) {
+  public static void main(String[] args) { // O(n) по времени, O(n) по памяти
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
-    Set<Integer> numbers = new HashSet<>();
+    Set<Integer> numbers = new HashSet<>(); // O(n) по памяти
     for (int i = 0; i < n; ++i) { // n раз, каждый раз по O(1), итого - O(1 * n) = O(n)
 //      int x = scanner.nextInt();
 //      numbers.add(x);
@@ -22,7 +22,8 @@ public class Task2Set {
 
     // удаление: 2 способа
     // 1-й способ: перебор копии
-//    Set<Integer> numbersCopy = new HashSet<>(numbers); // создаём копию numbers - O(n)
+//    Set<Integer> numbersCopy = new HashSet<>(numbers); // создаём копию numbers - O(n) по времени,
+//    // O(n) по памяти
 //    for (int x : numbersCopy) { // перебираем копию - n элементов, для каждого O(1), итого O(n)
 //      if (x % 2 != 0) { // O(1)
 //        numbers.remove(x); // меняем оригинал // O(1)
@@ -30,7 +31,7 @@ public class Task2Set {
 //    }
     // O(n) + O(2 * n) = O(3 * n) = O(n)
     // 2-й способ: временное хранилище для всего, что надо удалить
-    Set<Integer> oddNumbersToRemove = new HashSet<>(); // O(1)
+    Set<Integer> oddNumbersToRemove = new HashSet<>(); // O(1) по времени, O(n / 2) = O(n) по памяти
     for (int x : numbers) { // перебираем оригинал // O(1 * n) = O(n)
       if (x % 2 != 0) { // если число нечётное // O(1)
         oddNumbersToRemove.add(x); // меняем временную коллекцию // O(1)
