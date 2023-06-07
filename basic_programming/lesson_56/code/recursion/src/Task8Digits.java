f23d91cbc00d19b32d180fa63212b307b9c6a7a4
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-07 20:56:56 +0200

    feat: add Task8Digits.binary()

--- basic_programming/lesson_56/code/recursion/src/Task8Digits.java
+++ basic_programming/lesson_56/code/recursion/src/Task8Digits.java
@@ -2,6 +2,7 @@ public class Task8Digits {
 
   public static void main(String[] args) {
     System.out.println(sum(123));
+    System.out.println(binary(128));
   }
 
   // сумма цифр числа
@@ -12,5 +13,18 @@ public class Task8Digits {
     int last = x % 10;
     int rest = x / 10; // всё, кроме последней цифры
     return last + sum(rest);
+  }
+
+  public static String binary(int x) {
+    if (x < 0) {
+      throw new IllegalArgumentException("Число должно быть положительным: " + x);
+    }
+    // если число меньше 2, то в двоичной системе оно такое же
+    if (x < 2) {
+      return String.valueOf(x);
+    }
+    // в остальных случаях:
+    // "результат деления на два" - левая часть и "остаток от деления на 2" - последняя цифра
+    return binary(x / 2) + x % 2;
   }
 }
