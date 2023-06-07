01b680886c9b9160056086366a3817094cb33c0f
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-07 17:43:28 +0200

    refactor: replace similar code with Map usage

--- basic_programming/lesson_56/code/recursion/src/homework_55/Task1Brackets.java
+++ basic_programming/lesson_56/code/recursion/src/homework_55/Task1Brackets.java
@@ -2,6 +2,8 @@ package homework_55;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Task1Brackets {
@@ -37,57 +39,46 @@ public class Task1Brackets {
     System.out.println(correct);
   }
 
+  // Усовершенствовать код файла `Task1Brackets`, избавиться от дублирования кода.
+
   // сложность - O(n) по времени, максимум O(n) по дополнительной памяти
   public static boolean checkBrackets(String brackets) {
+    Map<Character, Character> bracketsMap = new HashMap<>();
+    bracketsMap.put(')', '(');
+    bracketsMap.put('}', '{');
+    bracketsMap.put(']', '[');
+    bracketsMap.put('>', '<');
     Deque<Character> openedBrackets = new ArrayDeque<>(); // максимум n/2 памяти (скобок) = O(n)
 //    for (char c : brackets.toCharArray()) { // или по индексам и `brackets.charAt(i)`
     for (int i = 0; i < brackets.length(); ++i) { // .toCharArray - O(n) дополнительной памяти
-      char c = brackets.charAt(i);
-      switch (c) {
+      char curr = brackets.charAt(i);
+      switch (curr) {
         case '(':
         case '{':
         case '[':
         case '<':
-          openedBrackets.offerLast(c);
+          openedBrackets.offerLast(curr);
           break;
-        case ')': {
-//          if (openedBrackets.isEmpty()) {
-//            return false; // открывающих скобок нет, последовательность сломалась
-//          }
-          // poll одновременно убирает из стека и отдаёт нам
-          Character opened = openedBrackets.pollLast(); // если открывающих скобок нет, вернёт null
-          if (opened == null || !opened.equals('(')) { // если скобки нет или она не круглая
-            return false; // последовательность сломалась
-          }
-          break;
-        }
-        case '}': {
-          Character opened = openedBrackets.pollLast(); // если открывающих скобок нет, вернёт null
-          if (opened == null || !opened.equals('{')) { // если скобки нет или она не фигурная
-            return false; // последовательность сломалась
-          }
-          break;
-        }
-        case ']': {
-          Character opened = openedBrackets.pollLast(); // если открывающих скобок нет, вернёт null
-          if (opened == null || !opened.equals('[')) { // если скобки нет или она не квадратная
-            return false; // последовательность сломалась
-          }
-          break;
-        }
+        case ')':
+        case '}':
+        case ']':
         case '>': {
-          Character opened = openedBrackets.pollLast(); // если открывающих скобок нет, вернёт null
-          if (opened == null || !opened.equals('<')) { // если скобки нет или она не угловая
+          // poll одновременно убирает из стека и отдаёт нам
+          Character lastOpened = openedBrackets.pollLast(); // если открывающих скобок нет, null
+          // если скобки нет или она неправильная
+          // bracketsMap - "правильные" скобочки, по закрывающей получаем открывающую
+          if (lastOpened == null || !lastOpened.equals(bracketsMap.get(curr))) {
             return false; // последовательность сломалась
           }
           break;
         }
         default:
           // не скобка, можно выкинуть ошибку, вернуть false или проигнорировать
+          // мы решили проигнорировать
           break;
       }
     }
     // всё проверили и ни разу не сломались
-    return openedBrackets.isEmpty(); // если не осталось открытых и не закрытых скобок
+    return openedBrackets.isEmpty(); // если не осталось открытых (и не закрытых) скобок
   }
 }
