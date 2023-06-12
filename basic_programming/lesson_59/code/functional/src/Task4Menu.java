10801097eff3505358c851b5ca8f5d1d3e6f82ed
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-12 21:01:40 +0200

    feat: use Runnable

--- basic_programming/lesson_59/code/functional/src/Task4Menu.java
+++ basic_programming/lesson_59/code/functional/src/Task4Menu.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -10,6 +13,7 @@ public class Task4Menu {
   private static final String SORT = "3";
 
   private static final Map<String, String> descriptions = new LinkedHashMap<>();
+
   static {
     descriptions.put(ADD, "Добавить элемент");
     descriptions.put(PRINT, "Вывести элементы");
@@ -17,13 +21,53 @@ public class Task4Menu {
     descriptions.put(EXIT, "Выход");
   }
 
-  public static void print() {
+  // Функциональный интерфейс для методов, которые ничего не получают и ничего не возвращают:
+  // `Runnable`
+  private static final Map<String, Runnable> actions = new LinkedHashMap<>();
+
+  static {
+    actions.put(ADD, new Runnable() {
+      @Override
+      public void run() {
+        add();
+      }
+    });
+    actions.put(PRINT, new Runnable() {
+      @Override
+      public void run() {
+        System.out.println(numbers);
+      }
+    });
+    actions.put(SORT, new Runnable() {
+      @Override
+      public void run() {
+        Collections.sort(numbers);
+      }
+    });
+    actions.put(EXIT, new Runnable() {
+      @Override
+      public void run() {
+        System.exit(0); // выход из программы
+      }
+    });
+  }
+
+  private static void add() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите целое число для добавления: ");
+    numbers.add(scanner.nextInt());
+  }
+
+  private static List<Integer> numbers = new ArrayList<>();
+
+  private static void print() {
     for (Map.Entry<String, String> entry : descriptions.entrySet()) {
       System.out.println(entry.getKey() + ". " + entry.getValue());
     }
   }
 
   public static String read() {
+    print();
     Scanner scanner = new Scanner(System.in);
     System.out.print("Введите команду: ");
     String command = scanner.nextLine();
@@ -33,5 +77,19 @@ public class Task4Menu {
       command = scanner.nextLine();
     }
     return command;
+  }
+
+  public static void apply(String command) {
+    if (!actions.containsKey(command)) {
+      throw new IllegalArgumentException("Некорректная команда: " + command);
+    }
+    Runnable action = actions.get(command);
+    action.run();
+  }
+
+  public static void main(String[] args) {
+    while (true) {
+      apply(read());
+    }
   }
 }
