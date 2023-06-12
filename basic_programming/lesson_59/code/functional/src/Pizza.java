314f986a7858e826a7114777f0b5d655fc3cfacf
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-12 18:00:26 +0200

    feat: add Pizza

---
+++ basic_programming/lesson_59/code/functional/src/Pizza.java
@@ -0,0 +1,46 @@
+public class Pizza {
+
+  String title;
+  int diameter;
+  int weight;
+  double price;
+
+  public Pizza(String title, int diameter, int weight, double price) {
+    this.title = title;
+    this.diameter = diameter;
+    this.weight = weight;
+    this.price = price;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public int getDiameter() {
+    return diameter;
+  }
+
+  public void setDiameter(int diameter) {
+    this.diameter = diameter;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+}
