fca1ba2e642a1388904eea97d8fd810a26d4f0e0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-14 20:11:08 +0200

    feat: improve Task2ComplexStream

--- basic_programming/lesson_61/code/streams/src/Task2ComplexStream.java
+++ basic_programming/lesson_61/code/streams/src/Task2ComplexStream.java
@@ -6,10 +6,11 @@ public class Task2ComplexStream {
   public static void main(String[] args) {
     int[] numbers = {4, 2, 3, 1, 5};
     String result = Arrays.stream(numbers)
-        .sorted()
-        .map(x -> x * x)
-        .mapToObj(x -> Integer.toString(x))
-        .collect(Collectors.joining(", "));
+        .filter(x -> x % 2 != 0)            // оставляем нечётные
+        .sorted()                           // сортируем
+        .map(x -> x * x)                    // превращаем в квадраты
+        .mapToObj(Integer::toString)        // превращаем IntStream в Stream<String>
+        .collect(Collectors.joining(", ")); // склеиваем через ", " в одну строку
     System.out.println(result);
   }
 }
