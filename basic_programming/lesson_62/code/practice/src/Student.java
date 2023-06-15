f302e355fc003e86126eb88c217e914c8561d475
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-15 18:45:58 +0200

    feat: add task description

---
+++ basic_programming/lesson_62/code/practice/src/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+  String name;
+  int score;
+
+  public Student(String name, int score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  @Override
+  public String toString() {
+    return name + " (score: " + score + ")";
+  }
+}
