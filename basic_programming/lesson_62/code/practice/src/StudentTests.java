fe4c9c5acced897f5546a5172c245a0fa94c4f13
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-06-15 20:21:30 +0200

    feat: add testCreation and testToString

--- basic_programming/lesson_62/code/practice/src/StudentTests.java
+++ basic_programming/lesson_62/code/practice/src/StudentTests.java
@@ -5,12 +5,31 @@ import org.junit.jupiter.api.Test;
 public class StudentTests {
 
   @Test
-  public void constructor() {
+  public void testCreation() {
+    String name = "Test";
+    int score = 42;
+    Student student = new Student(name, score);
+    assertEquals(name, student.getName());
+    assertEquals(score, student.getScore());
+
+    name = "Test2";
+    student.setName(name);
+    assertEquals(name, student.getName());
+
+    score *= 2;
+    student.setScore(score);
+    assertEquals(score, student.getScore());
+  }
+
+  @Test
+  public void testToString() {
     String name = "Test";
     int score = 42;
     Student student = new Student(name, score);
 
-    assertEquals(name, student.getName());
-    assertEquals(score, student.getScore());
+    String expected = name + " (score: " + score + ")";
+    String line = student.toString();
+
+    assertEquals(expected, line);
   }
 }
