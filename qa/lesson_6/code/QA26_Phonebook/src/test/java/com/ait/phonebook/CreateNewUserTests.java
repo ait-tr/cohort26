9e7b39da8f1b17c201d8b0e03389f361f8016ddb
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2023-08-31 13:36:46 +0200

    Lesson_6

--- qa/lesson_6/code/QA26_Phonebook/src/test/java/com/ait/phonebook/CreateNewUserTests.java
+++ qa/lesson_6/code/QA26_Phonebook/src/test/java/com/ait/phonebook/CreateNewUserTests.java
@@ -2,25 +2,28 @@ package com.ait.phonebook;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CreateNewUserTests extends TestBase{
 
+    @BeforeMethod
+    public void ensurePrecondition() {
+        //precondition: user should be logged out
+        if (!isLoginLinkPresent()) {
+            clickOnSignOutButton();
+        }
+        //click on login link
+        clickOnLoginLink();
+    }
+
     @Test
-    public void createNewUserPositiveTest() {
-        //click on login link - //a[contains(.,'LOGIN')] - xpath
-        driver.findElement(By.xpath("//a[contains(.,'LOGIN')]")).click();
-        //enter email - [placeholder='Email'] - css
-        driver.findElement(By.cssSelector("[placeholder='Email']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).sendKeys("stark@gmail.com");
-        //enter password - [placeholder='Password'] - css
-        driver.findElement(By.cssSelector("[placeholder='Password']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Password']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Password']")).sendKeys("Stark1234$");
-        //click on Registration button - registration - name
-        driver.findElement(By.name("registration")).click();
-        //assert Sign out button present - //button[contains(.,'Sign Out')] - xpath
-        Assert.assertTrue(isElementPresent(By.xpath("//button[contains(.,'Sign Out')]")));
+    public void createExistedUserNegativeTest() {
+        //enter email & password
+        fillLoginRegisterForm();
+        //click on Registration button
+        clickOnRegistrationButton();
+        //assert Sign out button present
+        Assert.assertTrue(isAlertPresent());
     }
 }
