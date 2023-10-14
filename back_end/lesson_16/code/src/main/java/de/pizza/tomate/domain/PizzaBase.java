00f014bda78cd63e5e7d381e4c0183482f9aa4af
Sergey Iryupin <biblelamp@gmail.com>
2023-10-14 06:04:07 +0200

    back_end lesson #16 init

---
+++ back_end/lesson_16/code/src/main/java/de/pizza/tomate/domain/PizzaBase.java
@@ -0,0 +1,37 @@
+package de.pizza.tomate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PizzaBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pizza_base_id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "pizza_type_id")
+    private PizzaType pizzaType;
+
+    @ManyToOne
+    @JoinColumn(name = "pizza_size_id")
+    private PizzaSize pizzaSize;
+
+    private Double price;
+}
